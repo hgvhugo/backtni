8cffc2e3cb147b2ffa01332460129f54b9b52aa4
[Cesar <[cicinhocarmona07@gmail.com]>
2023-12-21 13:49:32 -0600

    Codigo base Back TNI

---
+++ tni/src/main/java/mx/bidgroup/tec/tni/TniApplication.java
@@ -0,0 +1,13 @@
+package mx.bidgroup.tec.tni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TniApplication.class, args);
+	}
+
+}
---
+++ tni/src/test/java/mx/bidgroup/tec/tni/TniApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.bidgroup.tec.tni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
