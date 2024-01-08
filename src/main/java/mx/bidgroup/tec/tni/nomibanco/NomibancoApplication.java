0f911440cbcec29d85e290c4a348e911ddbe3b42
[Cesar <[cicinhocarmona07@gmail.com]>
2024-01-08 01:04:55 -0600

    CodigoBase

---
+++ src/main/java/mx/bidgroup/tec/tni/nomibanco/NomibancoApplication.java
@@ -0,0 +1,13 @@
+package mx.bidgroup.tec.tni.nomibanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomibancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomibancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/bidgroup/tec/tni/nomibanco/NomibancoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.bidgroup.tec.tni.nomibanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomibancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
